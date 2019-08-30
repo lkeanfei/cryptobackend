d966a2658adf929e07dfe6354361776f1c155e13
KeanFei LEONG <KeanFei.LEONG@3ds.com>
2019-08-30 18:14:15 +0800

    Added Technicals Summary

--- analysis/urls.py
+++ analysis/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^hello/' ,views.HelloApiView.as_view()),
     url(r'frontpage/', views.FrontPageView.as_view()),
+    url(r'technicalssummary/', views.TechnicalsSummaryView.as_view()),
     url(r'hourlydata/', views.HourlyDataView.as_view())
 
 ]
--- analysis/views.py
+++ analysis/views.py
@@ -22,6 +22,17 @@ def index(request):
 class Utils:
 
     @staticmethod
+    def getDictKeys(dictList):
+
+        keyList= []
+        for dict in dictList:
+            keyList.append(dict["key"])
+
+        return keyList
+
+
+
+    @staticmethod
     def filterKeys(dictList , requiredKeys):
 
         finalList = []
@@ -43,7 +54,6 @@ class Utils:
                     entryDict[requiredKey] = valueTypeDict
                 finalList.append(entryDict)
 
-            logger.info(finalList)
 
 
         return finalList
@@ -67,9 +77,9 @@ class FrontPageView(APIView):
                 else:
                     bearishCnt = bearishCnt + 1
 
-            maIndicatorList.append( {"coinpair" : technicals["coinpair"] , "bullishcnt" : bullishCnt , "bearishcnt" : bearishCnt})
+            maIndicatorList.append( {"exchange" : technicals["exchange"] , "coinpair" : technicals["coinpair"] , "bullishcnt" : bullishCnt , "bearishcnt" : bearishCnt})
 
-            logger.info(technicals["coinpair"] + " Bullish " + str(bullishCnt) + " Bearish " + str(bearishCnt))
+            # logger.info(technicals["coinpair"] + " Bullish " + str(bullishCnt) + " Bearish " + str(bearishCnt))
 
         return maIndicatorList
 
@@ -80,6 +90,10 @@ class FrontPageView(APIView):
 
         print("Printing inside Front page view")
         logger.info('Inside post Front page view')
+        logger.info(request.method)
+        logger.info(request.POST)
+        logger.info(request.data.keys())
+
 
         tradingStartTimeDict = Tradingtime.objects.order_by('-starttime').values('starttime').first()
 
@@ -95,7 +109,7 @@ class FrontPageView(APIView):
                                                                                            "sma10bullish", "sma20bullish" , "sma50bullish" ,
                                                                                            "dema10bullish", "dema20bullish" , "dema50bullish" ,
                                                                                            "wma10bullish", "wma20bullish" , "wma50bullish" ,
-                                                                                           "macdindicator")
+                                                                                           "macdindicator" , "exchange")
 
         logger.info('Finished getting technicals')
 
@@ -130,65 +144,242 @@ class FrontPageView(APIView):
         sortedBearishMAList = sorted(maIndicatorList , key=lambda k:k["bearishcnt"] ,reverse=True).copy()
 
         bearishMAHeaders = [
+            {"key": "exchange", "name": "Exchange"},
             {"key": "coinpair", "name": "Coin Pair"},
             {"key": "bearishcnt", "name": "Bearish Indicators"}
         ]
 
         bullishMAHeaders = [
+            {"key": "exchange", "name": "Exchange"},
             {"key": "coinpair", "name": "Coin Pair"},
             {"key": "bullishcnt", "name": "Bullish Indicators"}
         ]
 
         topgainersHeaders = [
+            { "key" : "exchange" , "name" : "Exchange" },
             { "key" : "coinpair" , "name" : "Coin Pair" } ,
             {"key": "pricechangepct", "name": "Price Change (%)"}
         ]
 
         toplosersHeaders = [
+            {"key": "exchange", "name": "Exchange"},
             {"key": "coinpair", "name": "Coin Pair"},
             {"key": "pricechangepct", "name": "Price Change (%)"}
         ]
 
         unusualVolumeHeaders = [
+            {"key": "exchange", "name": "Exchange"},
             {"key": "coinpair", "name": "Coin Pair"},
             {"key": "unusualvolume", "name": "Unusual Volume"}
         ]
 
         overboughtHeaders  = [
+            {"key": "exchange", "name": "Exchange"},
             {"key": "coinpair", "name": "Coin Pair"},
             {"key": "rsi", "name": "RSI"}
         ]
 
         oversoldHeaders = [
+            {"key": "exchange", "name": "Exchange"},
             {"key": "coinpair", "name": "Coin Pair"},
             {"key": "rsi", "name": "RSI"}
         ]
 
         macdHeaders = [
+            {"key": "exchange", "name": "Exchange"},
             {"key": "coinpair", "name": "Coin Pair"},
             {"key": "macdindicator", "name": "Bullish/Bearish"}
         ]
 
+        bmaList =  Utils.getDictKeys(bearishMAHeaders)
+        print(bmaList)
+
         response = {"frontpage": tradingStartTime ,
-                    "bearishma" : Utils.filterKeys( sortedBearishMAList[:10] , ["coinpair" , "bearishcnt"]),
+                    "bearishma" : Utils.filterKeys( sortedBearishMAList[:10] , bmaList),
                     "bearishmaheaders" : bearishMAHeaders,
-                    "bullishma": Utils.filterKeys(sortedBullishMAList[:10], ["coinpair", "bullishcnt"]),
+                    "bullishma": Utils.filterKeys(sortedBullishMAList[:10], Utils.getDictKeys(bullishMAHeaders)),
                     "bullishmaheaders": bullishMAHeaders,
-                    "macd" : Utils.filterKeys( macdList , ["coinpair" , "macdindicator"]),
+                    "macd" : Utils.filterKeys( macdList , Utils.getDictKeys(macdHeaders)),
                     "macdheaders": macdHeaders,
-                    "topgainers" : Utils.filterKeys(topgainers[:10] , [ "coinpair" , "pricechangepct" ]) ,
+                    "topgainers" : Utils.filterKeys(topgainers[:10] , Utils.getDictKeys(topgainersHeaders)) ,
                     "topgainersheaders" : topgainersHeaders,
-                    "toplosers" : Utils.filterKeys(toplosers[:10] , ["pricechangepct" , "coinpair"]) ,
+                    "toplosers" : Utils.filterKeys(toplosers[:10] , Utils.getDictKeys(toplosersHeaders)) ,
                     "toplosersheaders" : toplosersHeaders,
-                    "unusualvolume": Utils.filterKeys(unusualVolume[:10] , ["unusualvolume" , "coinpair"]) ,
+                    "unusualvolume": Utils.filterKeys(unusualVolume[:10] ,Utils.getDictKeys(unusualVolumeHeaders)) ,
                     "unusualvolumeheaders" : unusualVolumeHeaders,
-                    "overbought" : Utils.filterKeys(sortedOverBoughtList[:10] , ["rsi" , "coinpair"]) ,
+                    "overbought" : Utils.filterKeys(sortedOverBoughtList[:10] , Utils.getDictKeys(overboughtHeaders)) ,
                     "overboughtheaders" : overboughtHeaders,
-                    "oversold" : Utils.filterKeys(sortedOverSoldList[:10] , ["rsi" , "coinpair"]) ,
+                    "oversold" : Utils.filterKeys(sortedOverSoldList[:10] , Utils.getDictKeys(oversoldHeaders)) ,
                     "oversoldheaders" : oversoldHeaders
                     }
 
         return Response(response)
+
+
+
+class TechnicalsSummaryView(APIView):
+
+    def calcMAIndicators(self  , allTechnicalsList):
+
+        keys = [ "ema10bullish", "ema20bullish" , "ema50bullish" , "sma10bullish", "sma20bullish" , "sma50bullish" ,
+                 "dema10bullish", "dema20bullish" , "dema50bullish" ,  "wma10bullish", "wma20bullish" , "wma50bullish" ]
+
+        maIndicatorList = []
+
+        for technicals in allTechnicalsList:
+            bullishCnt = 0
+            bearishCnt = 0
+            for key in keys:
+
+                if technicals[key] == 1:
+                    bullishCnt = bullishCnt + 1
+                else:
+                    bearishCnt = bearishCnt + 1
+
+            maIndicatorList.append( {"exchange" : technicals["exchange"] , "coinpair" : technicals["coinpair"] , "bullishcnt" : bullishCnt , "bearishcnt" : bearishCnt})
+
+            # logger.info(technicals["coinpair"] + " Bullish " + str(bullishCnt) + " Bearish " + str(bearishCnt))
+
+        return maIndicatorList
+
+
+
+
+    def post(self , request):
+
+        exchange = ""
+        exchangeList = ["Bitfinex", "BitTrex" , "Binance" , "OKEx" , "DigiFinex" , "HitBTC" , "Kucoin" , "Huobi Global"]
+        response = {}
+        if "exchange" in request.data.keys():
+            exchange = request.data["exchange"]
+
+            if exchange in exchangeList:
+
+                print("Technicals Summary " + exchange )
+
+                tradingStartTimeDict = Tradingtime.objects.order_by('-starttime').values('starttime').first()
+
+                logger.info('Trading start Time')
+                logger.info(tradingStartTimeDict)
+
+                tradingStartTime = tradingStartTimeDict["starttime"]
+
+                logger.info('Finished getting technicals')
+                technicalskwargs = {}
+                technicalskwargs["starttime"] = tradingStartTime
+                technicalskwargs["exchange"] = exchange
+
+                allTechnicals =  Technicals.objects.filter(**technicalskwargs).values("pricechangepct" , "coinpair" , "unusualvolume" , "rsi",
+                                                                                                   "ema10bullish", "ema20bullish" , "ema50bullish" ,
+                                                                                                   "sma10bullish", "sma20bullish" , "sma50bullish" ,
+                                                                                                   "dema10bullish", "dema20bullish" , "dema50bullish" ,
+                                                                                                   "wma10bullish", "wma20bullish" , "wma50bullish" ,
+                                                                                                   "macdindicator" , "exchange")
+
+                logger.info('Finished getting technicals')
+
+
+                topgainers = sorted(allTechnicals , key= lambda k:k["pricechangepct"] , reverse=True).copy()
+                toplosers = sorted(allTechnicals,key= lambda k:k["pricechangepct"]  ).copy()
+                unusualVolume = sorted(allTechnicals, key= lambda k:k["unusualvolume"], reverse=True).copy()
+
+                bullishMACDList =  [x for x in allTechnicals if x['macdindicator'] == 'Bullish']
+                bearishMACDList = [x for x in allTechnicals if x['macdindicator'] == 'Bearish']
+
+                macdList = []
+                macdList.extend(bullishMACDList[:5])
+                macdList.extend(bearishMACDList[:5])
+
+                overboughtList = []
+                oversoldList = []
+
+                for technicals in allTechnicals:
+
+                    if technicals["rsi"] > 70:
+                        overboughtList.append(technicals)
+
+                    if technicals["rsi"] < 30:
+                        oversoldList.append(technicals)
+
+                sortedOverBoughtList = sorted(overboughtList , key=lambda k:k["rsi"] , reverse=True).copy()
+                sortedOverSoldList = sorted(oversoldList , key=lambda k:k["rsi"]).copy()
+
+                maIndicatorList = self.calcMAIndicators(allTechnicals)
+                sortedBullishMAList = sorted(maIndicatorList , key=lambda k:k["bullishcnt"] ,reverse=True).copy()
+                sortedBearishMAList = sorted(maIndicatorList , key=lambda k:k["bearishcnt"] ,reverse=True).copy()
+
+                bearishMAHeaders = [
+                    {"key": "exchange", "name": "Exchange"},
+                    {"key": "coinpair", "name": "Coin Pair"},
+                    {"key": "bearishcnt", "name": "Bearish Indicators"}
+                ]
+
+                bullishMAHeaders = [
+                    {"key": "exchange", "name": "Exchange"},
+                    {"key": "coinpair", "name": "Coin Pair"},
+                    {"key": "bullishcnt", "name": "Bullish Indicators"}
+                ]
+
+                topgainersHeaders = [
+                    { "key" : "exchange" , "name" : "Exchange" },
+                    { "key" : "coinpair" , "name" : "Coin Pair" } ,
+                    {"key": "pricechangepct", "name": "Price Change (%)"}
+                ]
+
+                toplosersHeaders = [
+                    {"key": "exchange", "name": "Exchange"},
+                    {"key": "coinpair", "name": "Coin Pair"},
+                    {"key": "pricechangepct", "name": "Price Change (%)"}
+                ]
+
+                unusualVolumeHeaders = [
+                    {"key": "exchange", "name": "Exchange"},
+                    {"key": "coinpair", "name": "Coin Pair"},
+                    {"key": "unusualvolume", "name": "Unusual Volume"}
+                ]
+
+                overboughtHeaders  = [
+                    {"key": "exchange", "name": "Exchange"},
+                    {"key": "coinpair", "name": "Coin Pair"},
+                    {"key": "rsi", "name": "RSI"}
+                ]
+
+                oversoldHeaders = [
+                    {"key": "exchange", "name": "Exchange"},
+                    {"key": "coinpair", "name": "Coin Pair"},
+                    {"key": "rsi", "name": "RSI"}
+                ]
+
+                macdHeaders = [
+                    {"key": "exchange", "name": "Exchange"},
+                    {"key": "coinpair", "name": "Coin Pair"},
+                    {"key": "macdindicator", "name": "Bullish/Bearish"}
+                ]
+
+                bmaList =  Utils.getDictKeys(bearishMAHeaders)
+                print(bmaList)
+
+                response = {"frontpage": tradingStartTime ,
+                            "bearishma" : Utils.filterKeys( sortedBearishMAList[:10] , bmaList),
+                            "bearishmaheaders" : bearishMAHeaders,
+                            "bullishma": Utils.filterKeys(sortedBullishMAList[:10], Utils.getDictKeys(bullishMAHeaders)),
+                            "bullishmaheaders": bullishMAHeaders,
+                            "macd" : Utils.filterKeys( macdList , Utils.getDictKeys(macdHeaders)),
+                            "macdheaders": macdHeaders,
+                            "topgainers" : Utils.filterKeys(topgainers[:10] , Utils.getDictKeys(topgainersHeaders)) ,
+                            "topgainersheaders" : topgainersHeaders,
+                            "toplosers" : Utils.filterKeys(toplosers[:10] , Utils.getDictKeys(toplosersHeaders)) ,
+                            "toplosersheaders" : toplosersHeaders,
+                            "unusualvolume": Utils.filterKeys(unusualVolume[:10] ,Utils.getDictKeys(unusualVolumeHeaders)) ,
+                            "unusualvolumeheaders" : unusualVolumeHeaders,
+                            "overbought" : Utils.filterKeys(sortedOverBoughtList[:10] , Utils.getDictKeys(overboughtHeaders)) ,
+                            "overboughtheaders" : overboughtHeaders,
+                            "oversold" : Utils.filterKeys(sortedOverSoldList[:10] , Utils.getDictKeys(oversoldHeaders)) ,
+                            "oversoldheaders" : oversoldHeaders
+                            }
+
+        return Response(response)
+
 
 
 class HourlyDataView(APIView):
